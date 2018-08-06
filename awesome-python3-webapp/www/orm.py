e40021d6e66373f3e57287ff596b1e013115ac93
lihaoran <lihaoran@xiaoyouzi.com>
2018-08-06 22:20:47 +0800

    学习廖雪峰的python web开发

---
+++ awesome-python3-webapp/www/orm.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# 
+
+import asyncio
+import logging
+
+import aiomysql
+
+def log(sql, args=()):
+	logging.info('SQL: %s' % sql)
+
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+	logging.info('create database connection pool....')
+	global __pool
+	__pool = yield from aiomysql.create_pool(
+		host = kw.get('host', 'localhost'),
+		port = kw.get('port', '3306'),
+		use = kw['root'],
+		password = kw['password'],
+		db = kw['db'],
+		charset = kw.get('charset', 'utf8'),
+		autocommit = kw.get('autocommit', True),
+		maxsize = kw.get('maxsize', 10),
+		minsize = kw.get('minsize', 1),
+		loop = loop
+	)
