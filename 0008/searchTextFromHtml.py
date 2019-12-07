e51f1c5128980c3e49cad90d9da37e30a6f006e9
HoRan <>
2019-12-07 14:51:02 +0800

    no message

---
+++ 0008/searchTextFromHtml.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: HoRan.li
+# @Date:   2019-12-05 23:44:23
+# @Last Modified by:   HoRan.li
+# @Last Modified time: 2019-12-05 23:44:39
+# @E-mail: laken_phil@163.com
+
+"""
+Function Information:
+从html里找出正文
+"""
